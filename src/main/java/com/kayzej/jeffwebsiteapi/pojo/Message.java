361c8a473291218d0f49226d56e4f7a4cbd46436
Jeffrey Kayzerman <jeffrey.kayzerman@gmail.com>
2024-07-11 23:00:54 -0400

    post working

--- src/main/java/com/kayzej/jeffwebsiteapi/pojo/Message.java
+++ src/main/java/com/kayzej/jeffwebsiteapi/pojo/Message.java
@@ -1,7 +1,51 @@
 package com.kayzej.jeffwebsiteapi.pojo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Message {
-  String firstName;
-  String lastName;
-  String message;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String firstName;
+  private String lastName;
+  private String message;
+
+  // Getters and setters
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
 }
--- src/main/java/com/kayzej/jeffwebsiteapi/web/VisitorController.java
+++ src/main/java/com/kayzej/jeffwebsiteapi/web/VisitorController.java
@@ -28,78 +28,20 @@ import jakarta.validation.Valid;
 @RestController
 public class VisitorController {
 
-    // @Autowired
-    // private VistorService visitorService;
-
-    @Operation(summary = "Hello World", description = "Hello World")
-    @ApiResponse(responseCode = "200", description = "successful hello world")
-    @GetMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> helloDefault() {
-        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
-    }
-
     @Operation(summary = "Hello World", description = "Hello World")
     @ApiResponse(responseCode = "200", description = "successful hello world")
     @GetMapping(value = "/api/helloworld", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> helloWorld() {
-        return new ResponseEntity<>("{\"message\": \"Hello, JSON!\"}", HttpStatus.OK);
+        return new ResponseEntity<>("{\"message\": \"Hello, from Tomcat!\"}", HttpStatus.OK);
     }
 
-    @Operation(summary = "Store Message", description = "Creates a contact from the provided payload")
+    @Operation(summary = "Store Message", description = "Creates a message from the provided payload")
     @ApiResponses(value = {
-    @ApiResponse(responseCode = "201", description = "Successful creation of message"),
-    @ApiResponse(responseCode = "400", description = "Bad request: unsuccessful update", 
-        content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            @ApiResponse(responseCode = "201", description = "Successful creation of message"),
+            @ApiResponse(responseCode = "400", description = "Bad request: unsuccessful update", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
-    @PostMapping(value = "/api/recordVisitor", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Message> createVisitor(@Valid @RequestBody Message
-    contact) {
-    // contactService.saveContact(contact);
-    return new ResponseEntity<>(contact, HttpStatus.CREATED);
+    @PostMapping(value = "/api/recordMessage", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<HttpStatus> recordMessage(@Valid @RequestBody Message message) {
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
-
-    // @Operation(summary = "Retrieves contacts", description = "Provides a list of
-    // all contacts")
-    // @ApiResponse(responseCode = "200", description = "Successful retrieval of
-    // contacts", content = @Content(array = @ArraySchema(schema =
-    // @Schema(implementation = Contact.class))))
-    // @GetMapping(value = "/contact/all", produces =
-    // MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<List<Contact>> getContacts() {
-    // List<Contact> contacts = contactService.getContacts();
-    // return new ResponseEntity<>(contacts, HttpStatus.OK);
-    // }
-
-    // @Operation(summary = "Get contact by Id", description = "Returns a contact
-    // based on an ID")
-    // @ApiResponses(value = {
-    // @ApiResponse(responseCode = "404", description = "Contact doesn't exist",
-    // content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-    // @ApiResponse(responseCode = "200", description = "Successful retrieval of
-    // contact", content = @Content(schema = @Schema(implementation =
-    // Contact.class))),
-    // })
-    // @GetMapping(value = "/contact/{id}", produces =
-    // MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<Contact> getContact(@PathVariable String id) {
-    // Contact contact = contactService.getContactById(id);
-    // return new ResponseEntity<>(contact, HttpStatus.OK);
-    // }
-
-    // @Operation(summary = "Create Contact", description = "Creates a contact from
-    // the provided payload")
-    // @ApiResponses(value = {
-    // @ApiResponse(responseCode = "201", description = "Successful creation of
-    // contact"),
-    // @ApiResponse(responseCode = "400", description = "Bad request: unsuccessful
-    // update", content = @Content(schema = @Schema(implementation =
-    // ErrorResponse.class)))
-    // })
-    // @PostMapping(value = "/contact", produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<Contact> createContact(@Valid @RequestBody Contact
-    // contact) {
-    // contactService.saveContact(contact);
-    // return new ResponseEntity<>(contact, HttpStatus.CREATED);
-    // }
-
 }
