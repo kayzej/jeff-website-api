4a7251bd5d0a4f8db118908ca545aea81ddefec7
Jeffrey Kayzerman <jeffrey.kayzerman@gmail.com>
2024-08-06 22:05:34 -0400

    turning off postgres

--- src/main/java/com/kayzej/jeffwebsiteapi/web/VisitorController.java
+++ src/main/java/com/kayzej/jeffwebsiteapi/web/VisitorController.java
@@ -1,16 +1,10 @@
 package com.kayzej.jeffwebsiteapi.web;
 
-import java.util.List;
-
-// import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,8 +36,7 @@ public class VisitorController {
     })
     @PostMapping(value = "/api/recordMessage", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HttpStatus> recordMessage(@Valid @RequestBody Message message) {
-        
-        
+
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
